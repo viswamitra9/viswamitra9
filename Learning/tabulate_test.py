3f87090f62cf233ff99b0e8aa4f9bebce8b12738
srini <srinivasoguri7@gmail.com>
2020-10-26 13:53:43 +0530

    Snowflake user and database management

--- Learning/tabulate_test.py
+++ Learning/tabulate_test.py
@@ -1,19 +1,4 @@
-import sys
-from cryptography.fernet import Fernet
+import datetime
+from datetime import datetime
 
-try:
-    # encrypting
-    password = 'Tejpass'
-    key = Fernet.generate_key()
-    print("key is ",key)
-    f = Fernet(key)
-    token = f.encrypt(password.encode('utf-8'))
-    print ("ENC("+token.decode(encoding='UTF-8')+")")
-    print('encrypted: {0}'.format(token))
-    # decrypting
-    f = Fernet(key)
-    output = f.decrypt(token)
-    output_decoded = output.decode('utf-8')
-    print('decrypted: {0}'.format(output_decoded))
-except Exception as e:
-    print("test failed : ", str(e))
+print(datetime.now().strftime("%d%b%Y%H%M%S"))
--- Snowflake/arcesium/snowflake/snowflake_database_backup.py
+++ Snowflake/arcesium/snowflake/snowflake_database_backup.py
@@ -1,36 +1,49 @@
+"""
+Owner       : oguri
+Description : This script helps in taking database backup using zero copy clones of database.
+"""
 import textwrap
 import argparse
 import logging
 from datetime import datetime
 import sys
-import snowflakeutil
+import arcesium.snowflake.snowflakeutil as snowflakeutil
 
 logfile = '/g/dba/logs/snowflake/snowflake_database_backup_{}.log'.format(datetime.now().strftime("%d-%b-%Y-%H-%M-%S"))
 logging.basicConfig(format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', filename=logfile, level=logging.INFO)
 
 
-def backup_database(account, host, dbname, pod):
+def backup_database(account, pod, dbname):
+    """
+    Description : This method is to take the backup of the databases in a pod (account), if the dbname is all then it
+    will take backup of all database except internal databases
+    Args:
+        account: name of the account ex: arc1000
+        pod: name of the pod ex: terra
+        dbname: name of database arcesium_data_warehouse or all
+    """
     try:
-        backup_name = "backup_{}_{}".format(dbname, datetime.now().strftime("%d%b%Y%H%M%S"))
         logging.info("Creating DBA connection")
-        sa_pass = get_user_password('/secret/v2/snowflake/{}/db/sa'.format(pod))
-        dba_conn, dba_cur = get_dba_connection(account, host, 'sa', sa_pass)
-        logging.info("Created a database connection")
+        dba_conn, dba_cur = snowflakeutil.get_admin_connection(account, pod)
+        logging.info("Created super user connection")
         dba_cur.execute("use role accountadmin")
-        dba_cur.execute("select count(*) from information_schema.databases where lower(database_name)='{}';".format(
-            str(dbname).lower()))
-        result = dba_cur.fetchall()
-        for db in result:
-            if db[0] == 0:
-                logging.error("Database {} does not exist in pod {}".format(dbname, pod))
-                return
-        dba_cur.execute("create database {} clone {}".format(backup_name, dbname))
-        logging.info("Backup {} created successfully for {} in pod {}".format(backup_name, dbname, pod))
+        if dbname == 'all':
+            dba_cur.execute("select database_name from information_schema.databases where lower(database_name) "
+                            "not in ('audit_archive','demo_db','snowflake','snowflake_sample_data','util_db') and"
+                            "lower(database_name) not like 'backup_%'")
+            result = dba_cur.fetchall()
+            for db in result:
+                backup_name = "backup_{}_{}".format(db, datetime.now().strftime("%d%b%Y"))
+                dba_cur.execute("create database if not exists {} clone {}".format(backup_name, db))
+                logging.info("Backup {} created successfully for database {} in pod {}".format(backup_name, db, pod))
+        backup_name = "backup_{}_{}".format(dbname, datetime.now().strftime("%d%b%Y"))
+        dba_cur.execute("create database if not exists {} clone {}".format(backup_name, dbname))
+        logging.info("Backup {} created successfully for database {} in pod {}".format(backup_name, dbname, pod))
     except Exception as e:
-        raise Exception("Failed to take backup of database {} in pod {} with error : {}".format(dbname, pod, str(e)))
+        raise Exception("Failed to take backup of database in pod {} with error : {}".format(pod, str(e)))
 
 
-# parse the infput arguments
+# parse the input arguments
 def parse_arguments():
     # take input arguments and parse
     parser = argparse.ArgumentParser(add_help=True, formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -39,6 +52,8 @@ def parse_arguments():
     sudo -u sqlexec python snowflake_database_backup.py --pod baamuat --dbname arcesium_data_warehouse
                                   OR
     sudo -u sqlexec python snowflake_database_backup.py --env prod --dbname arcesium_data_warehouse
+                                  OR
+    sudo -u sqlexec python snowflake_database_backup.py --pod baamuat --dbname all
     '''))
     # Instances on which we need to perform the task
     inst = parser.add_mutually_exclusive_group(required=True)
@@ -53,7 +68,6 @@ def parse_arguments():
 def main():
     args = parse_arguments()
     pod = args.pod
-    env = args.env
     dbname = args.dbname
 
     instances = {}
@@ -65,36 +79,32 @@ def main():
         alert_key = "Snowflake database backup"
         alert_summary = "Snowflake database backup in pod {}".format(pod)
         # create SQL connection to get information about instances
-        cur_sql_dest, conn_sql_dest = sql_connect()
+        cur_sql_dest, conn_sql_dest = snowflakeutil.sql_connect()
         if args.pod:
-            query = "select lower(Host),lower(pod) from dbainfra.dbo.database_server_inventory " \
-                    "where lower(ServerType)='snowflake' and pod='{}' and IsActive=1".format(
-                args.pod)
+            query = "select lower(FriendlyName), lower(pod) from dbainfra.dbo.database_server_inventory " \
+                    "where lower(ServerType)='snowflake' and lower(pod)='{}' and IsActive=1".format(str(args.pod).lower())
             cur_sql_dest.execute(query)
             result = cur_sql_dest.fetchall()
             for instance in result:
                 instances[instance[0]] = instance[1]
 
         if args.env:
-            query = "select lower(Host),lower(pod) from dbainfra.dbo.database_server_inventory " \
-                    "where lower(ServerType)='snowflake' and lower(Env)='{}' and IsActive=1".format(
-                str(args.env).lower())
+            query = "select lower(FriendlyName), lower(pod) from dbainfra.dbo.database_server_inventory " \
+                    "where lower(ServerType)='snowflake' and lower(Env)='{}' and IsActive=1".format(str(args.env).lower())
             cur_sql_dest.execute(query)
             result = cur_sql_dest.fetchall()
             for instance in result:
                 instances[instance[0]] = instance[1]
         conn_sql_dest.close()
 
-        logging.info("Instances are {}".format(instances))
+        logging.info("Accounts to be backed up are {}".format(instances))
         # Run the backup function for all instances
-        for host in instances:
-            account = str(host).split('.')[0]
-            pod = instances[host]
-            logging.info("Taking backup of database {} in pod {}".format(dbname, pod))
-            backup_database(account, host, dbname, pod)
+        for account in instances:
+            pod = instances[account]
+            logging.info("Taking backup of database in pod {}".format(pod))
+            backup_database(account, pod, dbname)
     except Exception as e:
-        alert_description = "Failed to take database {} backup from pod {}, check logfile {}".format(dbname, pod,
-                                                                                                     logfile)
+        alert_description = "Failed to take database {} backup from pod {}, check logfile {}".format(dbname, pod,logfile)
         #radarutil.raise_radar_alert(alert_source, alert_severity, alert_class, alert_key, alert_summary,alert_description)
         sys.exit(1)
 
