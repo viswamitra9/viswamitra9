49636798e0dc35f22d7e08296e0e1b70fcd5ebcc
Viswamitra <pgclass07@gmail.com>
2021-04-05 23:18:44 +0530

    Delete Learning directory

--- Learning/tabulate_test.py
+++
@@ -1,5 +0,0 @@
-def test(id):
-    id = id + 100
-    print(id)
-
-test(200)
--- Learning/test.py
+++
@@ -1,9 +0,0 @@
-def test(A):
-    if min(A) < -1000000 or min(A) > 1000000:
-        raise Exception("Range out of value")
-    start = min(A)+1
-    while(start in A):
-        start = start + 1
-    print(start)
-
-test([-1,2,3])
